fbb03edd32ef0403250740d8fc1eb297ccb0ad21
Gleb Karpushkin <rugleb@gmail.com>
2019-05-02 13:45:40 +0300

    created CubeElement class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-#include "src/Element.h"
+#include "src/CubeElement.h"
+#include "src/LinearElement.h"
 
 int main(int argc, char* argv[])
 {
+    double length = 1.;
+
+    auto cube = new CubeElement(length);
+    auto linear = new LinearElement(length);
+
     return 0;
 }
