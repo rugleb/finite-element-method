14a8d288d85ca2e307010cd34463d62df47586b3
Gleb Karpushkin <rugleb@gmail.com>
2019-05-02 13:56:19 +0300

    defined element length from args

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include <string>
+
 #include "src/CubeElement.h"
 #include "src/LinearElement.h"
 
+
+#define LEFT_BORDER     1.
+#define RIGHT_BORDER    19.
+
+
 int main(int argc, char* argv[])
 {
-    double length = 1.;
+    // number of finite elements
+    int elements = argc > 1 ? std::stoi(argv[1]) : 10;
+
+    // length of finite element
+    double length = (RIGHT_BORDER - LEFT_BORDER) / elements;
 
     auto cube = new CubeElement(length);
     auto linear = new LinearElement(length);
